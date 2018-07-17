b1d8dbafff65fc40c59021b5742b37a4f8993165
Kira Hartness <kirahartness@gmail.com>
2018-07-17 14:02:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 get '/name' do
-  "My name is _"
+  "My name is Kira"
  end 
   
   get '/hometown' do
-    "My hometown is _"
+    "My hometown is Cambridge"
   end
   get '/favorite-song' do
-    "My favorite song is _"
+    "My favorite song is Wonderwall"
   end
   
   class App < Sinatra::Base
